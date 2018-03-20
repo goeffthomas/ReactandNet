717e61dd3fcb547286b010abbbeb84392f8bdee4
Goeff Thomas <goeff@starbly.com>
2018-03-19 22:17:03 -0700

    Initial setup

---
+++ Frontend/index.js
@@ -0,0 +1,13 @@
+﻿// Navigate to http://localhost:57125
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+const baseUrl = 'http://localhost:56674';
+
+console.log('page loaded');
+
+axios.get(`${baseUrl}/api/values`).then(response => {
+    console.log(response);
+    document.getElementById('app').innerText = JSON.stringify(response);
+})
